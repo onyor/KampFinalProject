02c1a050dd540ccf24af64daab1120866be96cf8
ONUR YILDIZ <20050361005@ybu.edu.tr>
2021-02-23 00:10:50 +0300

    Commit

--- Core/Utilities/Results/ErrorDataResult.cs
+++ Core/Utilities/Results/ErrorDataResult.cs
@@ -4,23 +4,23 @@ using System.Text;
 
 namespace Core.Utilities.Results
 {
-    public class ErrorDataResults<T> : DataResult<T>
+    public class ErrorDataResult<T> : DataResult<T>
     {
-        public ErrorDataResults(T data, string message) : base(data, false, message)
+        public ErrorDataResult(T data, string message) : base(data, false, message)
         {
 
         }
-        public ErrorDataResults(T data) : base(data, false)
+        public ErrorDataResult(T data) : base(data, false)
         {
 
         }
         // Data yı boş döndürmek istersek
-        public ErrorDataResults(string message) : base(default, false, message)
+        public ErrorDataResult(string message) : base(default, false, message)
         {
 
         }
 
-        public ErrorDataResults() : base(default, false)
+        public ErrorDataResult() : base(default, false)
         {
 
         }
